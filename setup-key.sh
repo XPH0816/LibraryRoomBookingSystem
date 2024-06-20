6ee976673ba19e9f24dcd2eb7ce7e99711e0cc13
Lim Shi Song <limshisong123@gmail.com>
2024-06-21 00:01:22 +0800

    chore: Update .gitignore to ignore key files

--- setup-key.sh
+++ setup-key.sh
@@ -1,4 +1,3 @@
-@echo off
 ssh-keygen -t rsa -b 4096 -m PEM -f ./src-tauri/src/private.pem -N ""
 openssl rsa -in ./src-tauri/src/private.pem -pubout -outform PEM -out ./src-tauri/src/public.pem
 rm ./src-tauri/src/private.pem.pub
